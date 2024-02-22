78c2a2420c414fc987e1e8db319a81e18bace099
Airton Oliveira <airtonoliveira4@hotmail.com>
2024-02-22 18:40:25 -0300

    chore: change version name e code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
 
     defaultConfig {
         applicationId = "com.z1.comparaprecos"
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         multiDexEnabled = true
 
